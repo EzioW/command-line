c6f6ec3e845f4fe73beb1ee9249908b3e5652e74
于新哲 <>
2019-09-11 19:16:00 +0800

    finish config

--- src/test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-if (!fs.existsSync('./seraph')) {
-  fs.mkdirSync('./seraph');
-  const info = { 'feature/test': ['0.1.0'] };
-  fs.writeFileSync('./seraph/branchInfo.json', JSON.stringify(info), { encoding: 'utf-8' });
-}
